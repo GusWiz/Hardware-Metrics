efb6d113c08b46cb2cb535a4662a03dc2f26c9db
Gustavo <pow10@txstate.edu>
2025-04-15 11:35:34 -0500

    Create main file that should showcase the performance metrics for the devices active components

---
+++ metrics.py
@@ -0,0 +1,5 @@
+# find libray that import db or api that has data over pc components, and normal metrics
+# Find a libray that allows of GUI stats over active pc components
+# 
+
+#   This file shoud, compare both stats.
