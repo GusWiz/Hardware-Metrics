d83df1277d734ab62472aae3b3f1c292ab58ca47
Gustavo <pow10@txstate.edu>
2025-04-15 11:18:29 -0500

    Created class for Components that will be used to check the metrics of the device.

---
+++ HW_Components.py
@@ -0,0 +1,14 @@
+class Component:
+    # sets the hardware components based on user input parameters.
+    def setComponent(cpu, memory, ram, GPU = None):
+        memory = memory
+        cpu = cpu
+        ram = ram
+        GPU = GPU
+    # Funciton should be able to identify the product of the hardware components
+    # maybe a ml model and/or database of hardware components.
+    # Future Feature: if the user does not specify model/brand of component assume,
+    #   stock product. 
+    def hardware_specifics():
+        pass
+    
