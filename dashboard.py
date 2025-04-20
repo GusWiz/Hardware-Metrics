87f51860f256c2a5c512dd70391e006136e6f20b
Gustavo <pow10@txstate.edu>
2025-04-20 00:20:14 -0500

    Testing implementing real-time GPU utilization monitoring in Streamlit dashboard, GPUtil not being detected

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,29 @@
 #   This file shoud, compare both stats.
 
 import streamlit as st
+import psutil
+import GPUtil
+import time
 
 st.set_page_config(layout="wide", page_title="System Performance Dashboard")
 st.title('Real-Time System Performance Metrics')
+
+gpu_placeholder = st.empty()
+mem_placeholder = st.empty()
+
+quantum_interval = 1 # updates every 1 second
+
+while True:
+    gpus = GPUtil.getGPUs() # Gets GPU data
+    if gpus: # If it detects any gpu then proceed
+        gpu = gpus[0]
+        gpu_utilization = gpu.load * 100 # load returns from 0-1, so we must convert to percentage
+        with gpu_placeholder.container():
+            st.subheader(f'GPU Usage for {gpu.name}')
+            st.write(f'Utilization: {gpu_utilization}')
+            st.progress(int(gpu_utilization)) # displays the data in a progress bar
+    else:
+        with gpu_placeholder.container():
+            st.subheader('GPU Usage')
+            st.write('No NVIDIA GPU detected')
+time.sleep(quantum_interval)
